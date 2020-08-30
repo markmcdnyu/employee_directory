8c8f74035d354741a3b2dd9260fc35febaec2309
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-29 20:28:46 -0500

    added the imports for employee, app, and form.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Employee from "./components/employee";
+import employeeList from "./data/employeeList.json";
+import Form from "./components/form";
+
+class App extends Component {
+
+
+
+
+}
+
+export default App;
