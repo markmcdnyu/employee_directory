e2052d3cb8b567f8f3f51f13945c75b64ac12555
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-29 20:41:19 -0500

    added the handleinput change, handle form submit, and sortName functions

--- src/app.js
+++ src/app.js
@@ -4,7 +4,53 @@ import employeeList from "./data/employeeList.json";
 import Form from "./components/form";
 
 class App extends Component {
+    state = {
+        sorted: employeeList,
+        firstName: "",
+        lastName: ""
+    }
 
+    handleInputChange = event => {
+        let value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+
+        let findFirstName = new Array(...employeeList)
+        console.log(findFirstName)
+
+        let filtersReturnNewArrays = findFirstName.filter(employee => {
+            return employee.firstName.includes(this.state.firstName)
+                || employee.lastName.includes(this.state.firstName)
+        })
+        console.log(filtersReturnNewArrays)
+
+        this.setState({ sorted: filtersReturnNewArrays })
+
+        this.setState({
+            firstName: "",
+            lastName: ""
+        });
+    };
+
+    sortName = () => {
+        let unsorted = new Array(...employeeList)
+        console.log(unsorted)
+
+        let sortof = unsorted.sort((a, b) => {
+            console.log(a.firstName, b.lastName)
+            console.log(b.firstName, a.lastName)
+            return (a.firstName > b.firstName) ? 1 : ((b.firstName > a.firstName) ? -1 : 0)
+        })
+        console.log(sortof)
+        this.setState({ sorted: sortof })
+    }
 
 
 
